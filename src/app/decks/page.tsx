1e3c3630308ab0181d1cfc01d5bc6a445d836c62
GeorgeLxL <crownelf4@gmail.com>
2026-05-05 20:41:01 +0900

    add suspense

--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import DeckSearch from "@/components/DeckSearch";
 
 export default function DecksPage() {
   return (
     <main className="min-h-screen bg-gray-50">
-      <DeckSearch />
+      <Suspense>
+        <DeckSearch />
+      </Suspense>
     </main>
   );
 }
